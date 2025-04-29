9567479b43af98412daea654b299098a2a3c2ad0
Anshul Vishwakarma <72655193+anssec@users.noreply.github.com>
2025-04-29 10:48:47 +0530

    Delete OSINT-Infogather directory

--- OSINT-Infogather/backend/models/scopeSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const scope = new mongoose.Schema({
-  api: {
-    type: String,
-    required: true,
-  },
-});
-module.exports = mongoose.model("apiscopes", scope);
--- OSINT-Infogather/backend/routes/allSSlScans.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { testsslRun } = require("../controllers/testsslRun");
-const router = express.Router();
-
-router.post("/testssl/:id", testsslRun);
-
-module.exports = router;
